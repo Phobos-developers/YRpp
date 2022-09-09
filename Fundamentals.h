8335a97eaad5f897d2a8e4fc7040a8cfacfe962e
Trsdy <914157150@qq.com>
2022-09-10 02:00:22 +0800

    buildup related

--- Fundamentals.h
+++ Fundamentals.h
@@ -8,16 +8,19 @@ namespace Unsorted
 {
 	static const int& CurrentFrame = *reinterpret_cast<int*>(0xA8ED84);
 
-	// The length of a cell in isometric projection
+	// The length of a cell in its isometric projection
 	// If an object's Height is above this value it's considered as in-air
 	constexpr int CellHeight = 208;
 	// Leptons of a cell's diagonal /2 /sin(60deg)
 	// LeptonsPerCell *sqrt(2) /2 */ (sqrt(3)/2)
-	// 256 * sqrt(2/3) = 209
-	// with some precision loss
+	// 256 * sqrt(2/3)
 
 	// The height in the middle of a cell, which is therefore CellHeight/2
 	constexpr int LevelHeight = 104;
+	// The game actually calculated this one and multiply it by 2 for CellHeight
+	// cot(deg2rad(60)) * leptonsOfCellDiagonal /2
+	// tan(pi/2-pi/3) * sqrt(2 * 256^2) * 0.5
+	// sqrt(3)/3 * 362.03 *0.5
 
 	// Leptons per cell.
 	constexpr int LeptonsPerCell = 256;
