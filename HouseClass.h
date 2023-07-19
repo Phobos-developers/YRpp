f2ddfe2c3f217f785f180f0f2c6782dda639df02
Belonit <54427022+Belonit@users.noreply.github.com>
2023-07-19 22:17:28 +0300

    Update HouseClass, add IsMutualAllie and MakeObserver

--- HouseClass.h
+++ HouseClass.h
@@ -215,6 +215,15 @@ public:
 	virtual AbstractType WhatAmI() const RT(AbstractType);
 	virtual int	Size() const R0;
 
+	bool MakeObserver() const
+	{
+		if (HouseClass::CurrentPlayer != this)
+			return false;
+
+		HouseClass::Observer = const_cast<HouseClass*>(this);
+		return true;
+	}
+
 	bool IsAlliedWith(int idxHouse) const
 		//{ JMP_THIS(0x4F9A10); }
 	{
@@ -242,6 +251,11 @@ public:
 		//	{ JMP_THIS(0x4F9AF0); }
 	{
 		return this->IsAlliedWith(generic_cast<ObjectClass const*>(pAbstract));
+	}
+
+	inline bool IsMutualAllie(HouseClass const* pHouse) const
+	{
+		return this->Allies.Contains(pHouse->ArrayIndex) && pHouse->Allies.Contains(this->ArrayIndex);
 	}
 
 	void MakeAlly(int iHouse, bool bAnnounce)
@@ -426,7 +440,7 @@ public:
 	static void __fastcall LoadFromINIList(CCINIClass *pINI)
 		{ JMP_STD(0x5009B0); }
 
-	int GetSpawnPosition() {
+	int GetSpawnPosition() const {
 		const int currentIndex = this->ArrayIndex;
 		const int* houseIndices = ScenarioClass::Instance.HouseIndices;
 
@@ -713,7 +727,7 @@ public:
 		return this == Observer;
 	}
 
-	bool inline IsInitiallyObserver()
+	bool inline IsInitiallyObserver() const
 	{
 		return this->IsHumanPlayer && (this->GetSpawnPosition() == -1);
 	}
