ffb5f70fa8bb14537d4c904376e8bb62cdb27d7b
Ironhammer Std <2482911962@qq.com>
2025-10-16 14:05:32 +0800

    Add SystemData

--- CRT.h
+++ CRT.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include <wchar.h>
 
@@ -178,4 +178,12 @@ public:
 
 		static void __cdecl makepath(char* arg1 , const char* arg2, const char* arg3 ,  const char* arg4, const char* arg5)
 			{ JMP_STD(0x7C9FF0); }
+
+
+		//exit
+		static [[noreturn]] void __cdecl exit(int Code)
+			{ JMP_STD(0x7CBDDC); }
+
+		_onexit_t __cdecl onexit(_onexit_t Func)
+			{ JMP_STD(0x7C970C); }
 };
